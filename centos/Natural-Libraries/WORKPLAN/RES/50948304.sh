aeeb36cbcc086d5c0a08b911d2340af2b3470c14
Mathias Steul <Mathias.Steul@softwareag.com>
2025-02-11 11:16:47 +0100

    initial commit

---
+++ centos/Natural-Libraries/WORKPLAN/RES/50948304.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Command Script for adafdu
+#
+FDUFDT=/sag/nat9/Natural/tmp/Temp_NOP_FILE.FDT
+export FDUFDT
+/sag/ada7/Adabas/adafdu < /sag/nat9/Natural/tmp/Temp_NOP_FILE.FDU
+#
+# End of Command Script
